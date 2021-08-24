34c1a3a0aa7dd46240ea5e13af7639cb9783fb26
Juan Pablo <ortizrubio09@gmail.com>
2021-08-24 17:35:03 -0500

    Inicialización

---
+++ src/main/java/com/desarrolloweb/zathura/ZathuraApplication.java
@@ -0,0 +1,13 @@
+package com.desarrolloweb.zathura;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZathuraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZathuraApplication.class, args);
+	}
+
+}
