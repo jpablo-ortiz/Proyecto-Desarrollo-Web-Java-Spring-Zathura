46cc549e97bd5805363f0b03a90c27ab64e7e7d3
Juan Pablo <ortizrubio09@gmail.com>
2021-09-14 17:24:25 -0500

    Intento de union funcional

--- src/main/java/com/desarrolloweb/zathura/controllers/EstrellaController.java
+++ src/main/java/com/desarrolloweb/zathura/controllers/EstrellaController.java
@@ -1,80 +1,69 @@
 package com.desarrolloweb.zathura.controllers;
 
-import com.desarrolloweb.zathura.models.Estrella;
-import com.desarrolloweb.zathura.service.EstrellaService;
-import com.desarrolloweb.zathura.exception.RecordNotFoundException;
-
-
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.ui.Model;
+import com.desarrolloweb.zathura.exception.RecordNotFoundException;
+import com.desarrolloweb.zathura.models.Estrella;
+import com.desarrolloweb.zathura.service.EstrellaService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-
 import org.springframework.stereotype.Controller;
-
-import org.springframework.web.bind.annotation.RequestMapping;
-
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
-
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-
-
-
 
 @Controller
 @RequestMapping("/")
 public class EstrellaController {
 
-    //Logger log = LoggerFactory.getLogger(getClass());
+	Logger log = LoggerFactory.getLogger(getClass());
 
-    @Autowired
-    EstrellaService service;
-    //private EstrellaRepository estrellaRepository;
+	@Autowired
+	private EstrellaService service;
 
-    @RequestMapping
-    public String obtenerTodasLasEstrellas( Model model ){
-        
-		System.out.println("obtenerTodasLasEstrellas");
-       List <Estrella> lista = service.obtenerTodasLasEstrellas();
-        model.addAttribute("estrellas", lista);
-       return "listaEstrellas";
-    }
-    @RequestMapping(path = {"/editar", "/editar/{id}"})
-	public String editarEstrellaById(Model model, @PathVariable("id") Optional<Long> id) 
-							throws RecordNotFoundException 
-	{
-		
-		System.out.println("editarEstrellaById" + id);
+	@RequestMapping
+	public String obtenerTodasLasEstrellas(Model model) {
+		log.info("obtenerTodasLasEstrellas");
+		List<Estrella> lista = service.obtenerTodasLasEstrellas();
+		model.addAttribute("estrellas", lista);
+		return "listaEstrellas";
+	}
+
+	@RequestMapping(path = { "/editar", "/editar/{id}" })
+	public String editarEstrellaById(Model model, @PathVariable("id") Optional<Long> id)
+			throws RecordNotFoundException {
+
+		log.info("editarEstrellaById" + id);
 		if (id.isPresent()) {
 			Estrella entity = service.getEstrellaById(id.get());
 			model.addAttribute("estrella", entity);
 		} else {
 			model.addAttribute("estrella", new Estrella());
 		}
-		
+
 		return "crearEstrella";
 	}
-    @RequestMapping(path = "/eliminar/{id}")
-	public String eliminarEstrellaById(Model model, @PathVariable("id") Long id) 
-							throws RecordNotFoundException 
-	{
-		
-		System.out.println("eliminarEstrellaById" + id);
-		
+
+	@RequestMapping(path = "/eliminar/{id}")
+	public String eliminarEstrellaById(Model model, @PathVariable("id") Long id) throws RecordNotFoundException {
+
+		log.info("eliminarEstrellaById" + id);
+
 		service.eliminarEstrellaById(id);
 		return "redirect:/";
 	}
 
 	@RequestMapping(path = "/crearEstrella", method = RequestMethod.POST)
-	public String crearOactualizarEstrella(Estrella estrella) 
-	{
-		System.out.println("creando estrella ");
-		
+	public String crearOactualizarEstrella(Estrella estrella) {
+		log.info("creando estrella ");
+
 		service.crearOactualizarEstrella(estrella);
-		
+
 		return "redirect:/";
 	}
-
 
 }
--- src/main/java/com/desarrolloweb/zathura/service/EstrellaService.java
+++ src/main/java/com/desarrolloweb/zathura/service/EstrellaService.java
@@ -1,96 +1,91 @@
 package com.desarrolloweb.zathura.service;
 
-import com.desarrolloweb.zathura.repositories.EstrellaRepository;
-import com.desarrolloweb.zathura.models.Estrella;
-import com.desarrolloweb.zathura.exception.RecordNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+import com.desarrolloweb.zathura.exception.RecordNotFoundException;
+import com.desarrolloweb.zathura.models.Estrella;
+import com.desarrolloweb.zathura.repositories.EstrellaRepository;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
-public class EstrellaService{
- 
-    @Autowired
+public class EstrellaService {
+
+	Logger log = LoggerFactory.getLogger(getClass());
+
+	@Autowired
 	EstrellaRepository repository;
-	
-	public List<Estrella> obtenerTodasLasEstrellas()
-	{
-		System.out.println("obteniendo todas las estrellas");
+
+	public List<Estrella> obtenerTodasLasEstrellas() {
+		log.info("obteniendo todas las estrellas");
 		List<Estrella> resultado = (List<Estrella>) repository.findAll();
-		
-		if(resultado.size() > 0) {
+
+		if (resultado.size() > 0) {
 			return resultado;
 		} else {
 			return new ArrayList<Estrella>();
 		}
 	}
 
-	
-	public Estrella getEstrellaById(Long id) throws RecordNotFoundException 
-	{
-		System.out.println("obtener estrella por id");
+	public Estrella getEstrellaById(Long id) throws RecordNotFoundException {
+		log.info("obtener estrella por id");
 		Optional<Estrella> estrella = repository.findById(id);
-		
-		if(estrella.isPresent()) {
+
+		if (estrella.isPresent()) {
 			return estrella.get();
 		} else {
 			throw new RecordNotFoundException("No existe la estrella con dicha identificación");
 		}
 	}
-	
-	public Estrella crearOactualizarEstrella(Estrella entity) 
-	{
-		System.out.println("Creando o actualizando estrella");
-	 
-		if(entity.getId()  == null) 
-		{
+
+	public Estrella crearOactualizarEstrella(Estrella entity) {
+		log.info("Creando o actualizando estrella");
+
+		if (entity.getId() == null) {
 			entity = repository.save(entity);
-			
+
 			return entity;
-		} 
-		else 
-		{
-			// update existing entry 
+		} else {
+			// update existing entry
 			Optional<Estrella> estrella = repository.findById(entity.getId());
-			
-			if(estrella.isPresent()) 
-			{
+
+			if (estrella.isPresent()) {
 				Estrella nueva = estrella.get();
-                nueva.setNombre(entity.getNombre());
-                nueva.setHabitado(entity.getHabitado());
-                nueva.setRecurso(entity.getRecurso());
-               // nueva.setPlanetas(entity.getPlanetas());
-               // nueva.setRutasA(entity.getRutasA());
-               // nueva.setRutasB(entity.getRutasB());
-                nueva.setX(entity.getX());
-                nueva.setY(entity.getY());
-                nueva.setZ(entity.getZ());
-			    //Guardamos la nueva estrella
+				nueva.setNombre(entity.getNombre());
+				nueva.setHabitado(entity.getHabitado());
+				nueva.setRecurso(entity.getRecurso());
+				// nueva.setPlanetas(entity.getPlanetas());
+				// nueva.setRutasA(entity.getRutasA());
+				// nueva.setRutasB(entity.getRutasB());
+				nueva.setX(entity.getX());
+				nueva.setY(entity.getY());
+				nueva.setZ(entity.getZ());
+				// Guardamos la nueva estrella
 				nueva = repository.save(nueva);
-				
+
 				return nueva;
 			} else {
 				entity = repository.save(entity);
-				
+
 				return entity;
 			}
 		}
-	} 
-	
-	public void eliminarEstrellaById(Long id) throws RecordNotFoundException 
-	{
-		System.out.println("Eliminar Estrella por id");
-		
+	}
+
+	public void eliminarEstrellaById(Long id) throws RecordNotFoundException {
+		log.info("Eliminar Estrella por id");
+
 		Optional<Estrella> estrella = repository.findById(id);
-		
-		if(estrella.isPresent()) 
-		{
+
+		if (estrella.isPresent()) {
 			repository.deleteById(id);
 		} else {
 			throw new RecordNotFoundException("No se encontro la estrella con dicho id");
 		}
-	} 
+	}
 }
