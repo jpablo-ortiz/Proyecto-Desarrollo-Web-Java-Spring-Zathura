e0b03e5051f801eaa2181fbdfdc4840007498ba5
Juan Pablo Ortiz Rubio <45014827+lomitofrito@users.noreply.github.com>
2021-11-11 22:01:09 -0500

    Arreglo de configuracion para iniciar los test

--- src/test/java/com/desarrolloweb/zathura/EstrellaControllerIntegrationTest.java
+++ src/test/java/com/desarrolloweb/zathura/EstrellaControllerIntegrationTest.java
@@ -15,14 +15,17 @@ import com.desarrolloweb.zathura.repositories.PlanetaRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.ConfigFileApplicationContextInitializer;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
 
 @SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
-@ActiveProfiles("integrationtest")
+@ContextConfiguration(initializers = ConfigFileApplicationContextInitializer.class)
+@ActiveProfiles("test")
 // @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
 public class EstrellaControllerIntegrationTest {
 
@@ -47,8 +50,7 @@ public class EstrellaControllerIntegrationTest {
     @BeforeEach
     void init() {
         List<Estrella> estrellas = new ArrayList<Estrella>();
-        Estrella estrella1 = new Estrella("estrella1", Double.valueOf(2), Double.valueOf(4), Double.valueOf(5), 
-                true);
+        Estrella estrella1 = new Estrella("estrella1", Double.valueOf(2), Double.valueOf(4), Double.valueOf(5), true);
         Estrella estrella2 = new Estrella("estrella2", Double.valueOf(23), Double.valueOf(47), Double.valueOf(52),
                 true);
         Estrella estrella3 = new Estrella("estrella3", Double.valueOf(42), Double.valueOf(49), Double.valueOf(56),
@@ -69,17 +71,14 @@ public class EstrellaControllerIntegrationTest {
 
         planetaRepository.save(planeta1); // id5
 
-         // Crear Rutas de todas las estrellas con todas las estrellas
+        // Crear Rutas de todas las estrellas con todas las estrellas
         for (int i = 0; i < estrellas.size(); i++) {
             for (int j = i + 1; j < estrellas.size(); j++) {
                 Ruta ruta = new Ruta(estrellas.get(i), estrellas.get(j));
                 rutaController.crearRuta(ruta);
             }
-        }       
-    
-    
+        }
 
-        
     }
 
     @Test
